31b6cbc2b282c32f54cb318488534c40c3b9008e
Geneticanomaly <thomaskaatranen@gmail.com>
2024-09-09 16:43:23 +0300

    Fix linting errors part 2.

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,53 @@
-import HtmlWebPackPlugin from 'html-webpack-plugin'
-import path from 'path'
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
-export default [
-  {
-    entry: './src/index.jsx',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-      publicPath: '/',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
           },
-        },
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: 'html-loader',
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: 'style-loader',
-            },
-            {
-              loader: 'css-loader',
-            },
-          ],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['*', '.js', '.jsx'],
-    },
-    devServer: {
-      historyApiFallback: true,
-    },
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: './public/index.html',
-        filename: './index.html',
-      }),
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
     ],
   },
-  {
-    ignores: ['dist/**'],
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
   },
-]
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+}
